a6fa7fc272bd04c18a22d4a4c5956ae10d5f5e5d
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-03-31 17:07:52 +0200

    add drag-start event

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
 const listItems = document.querySelectorAll('li');
+const lists = document.querySelectorAll('ul');
 
 const setListItemsIds = () => {
   for (let listItem of listItems) {
-    let id = Math.random();
-    listItem.setAttribute('id', id);
+    listItem.setAttribute('id', Math.random());
   }
 };
+const connectDrag = () => {
+  document.addEventListener('dragstart', event => {
+    event.dataTransfer.setData('text/plain', event.srcElement.id);
+    event.dataTransfer.effectAllowed = 'move';
+  });
+};
 setListItemsIds();
+connectDrag();
